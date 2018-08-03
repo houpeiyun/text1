ae292752c66a2195ae9772dac9fd05a153543aae
houpeiyun <1659253829@qq.com>
2018-08-03 17:04:27 +0800

    scss

---
+++ es6/src/import.js
@@ -0,0 +1,11 @@
+// import {aaaa} from './export.js'
+// console.log(aaaa);
+// // console.log(foo(3,5));
+
+
+import bb from './export.js';
+console.log(bb);
+// let p1 = new bbb('zs', 18);
+// p1.showName();
+
+//输入babel-node import.js执行
