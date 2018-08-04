2e5866f68c7cf5afc6086a3c484989701f58754a
houpeiyun <1659253829@qq.com>
2018-08-04 17:03:52 +0800

    scss

---
+++ webpack/postcss.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    plugins:[
+       require('autoprefixer')
+    ],
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:ExtractTextPlugin.extract({
+                    fallback:"style-loader",
+                    use:[{
+                        loader:"css-loader",
+                        options:{importLoaders:1}
+                    },'postcss-loader'],
+                    publicPath:"../"
+                })
+            }
+        ]
+    }
+};
---
+++ webpack/src/index.js
@@ -0,0 +1,9 @@
+import css from './css/main.css'
+import sass from './css/index.scss'
+var a = 10;
+var box = document.getElementById("box");
+box.innerHTML = "abc";
+
+let foo = (name,age) => {
+    let aaa = name;
+}
